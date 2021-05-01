bb1fd2f32086c0e058e76e6b3ed6cfeffd0ef8e8
Debajyoti Majumdar <deb@Debajyotis-MacBook-Pro.local>
2021-05-02 00:43:52 +0530

    Basic exercises and code for understanding expressions.

---
+++ src/exercises/package.scala
@@ -0,0 +1,19 @@
+package object exercises  extends App{
+
+  // 1. difference between "hello world" vs println("hello world")
+  /*
+    "hello world" is a value of type string
+    println("hello world") returns Unit == void, it is a side effect
+   */
+
+  //2. What does the code below return?
+  val someValue = {
+    2 < 3
+  }
+  // true
+
+  val someOtherValue = {
+    if (someValue) 239 else 986
+  }
+  // 239
+}
--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -16,4 +16,38 @@ object Expressions extends App{
   aVariable += 3 // also works with -= *= /= .... side effects
   println(aVariable)
 
+  // Instructions DO things
+  // Expression return a VALUE
+
+  // IF expression: an IF condition which will return a value
+  val aCondition = true
+  val aConditionedValue = if(aCondition) 6 else -9  // IF expression
+  println(aConditionedValue)
+  println(if(aCondition) 6 else -9)
+
+
+  // there are loops in scala, however they are discouraged
+  // the loops are specific to java,python , etc..
+
+  // EVERYTHING in scala is an expression
+
+  val aWeirdValue = (aVariable = 3) // Unit == void
+  println(aWeirdValue)
+
+  // side effects in scala are expressions that return Unit/void
+  // these are: println(), while loops, reassigning
+
+  // Code blocks
+
+  val aCodeBlockValue = {
+    val x = 1
+    var y = x+2
+
+    if (y > 3) "hello" else "goodbye"
+  } // end of code block: these are also expressions
+
+  // instructions do something
+  // expressions evaluate something
+
+  print(aCodeBlockValue)
 }
